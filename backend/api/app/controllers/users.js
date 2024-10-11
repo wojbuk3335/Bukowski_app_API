53370140bb82308c35ead7d2104679eef5614d39
BUKOWSKI <w.bukowski1985@gmail.com>
2024-10-11 07:33:51 +0200

    Dodajemy wszystkie routingi i controllery

--- backend/api/app/controllers/users.js
+++ backend/api/app/controllers/users.js
@@ -1,6 +1,8 @@
 const User = require('../db/models/user');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const jsonwebtoken = require('../config').jsonwebtoken;
 
 class UsersController {
     signup(req, res, next) {
@@ -40,6 +42,119 @@ class UsersController {
                                     });
                                 });
                         }
+                    });
+                }
+            })
+            .catch(error => {
+                res.status(500).json({
+                    error: error
+                });
+            });
+    }
+    login(req, res, next){
+        User.findOne({ email: req.body.email })
+            .exec()
+            .then(user => {
+                if (!user) {
+                    return res.status(401).json({
+                        message: 'Auth failed'
+                    });
+                }
+                bcrypt.compare(req.body.password, user.password, (err, result) => {
+                    if (err) {
+                        return res.status(401).json({
+                            message: 'Auth failed'
+                        });
+                    }
+                    if (result) {
+                        const token = jwt.sign({
+                            email: user.email,
+                            userId: user._id
+                        }, jsonwebtoken, {
+                            expiresIn: '1h'
+                        });
+                        return res.status(200).json({
+                            message: 'Auth successful',
+                            token: token
+                        });
+                    }
+                    res.status(401).json({
+                        message: 'Auth failed'
+                    });
+                });
+            })
+            .catch(error => {
+                console.log(error);
+                res.status(500).json({
+                    error: error
+                });
+            });
+    }
+
+    deleteUser(req, res, next) {
+        const id = req.params.userId;
+        User.deleteOne({ _id: id })
+            .then(result => {
+                res.status(200).json({
+                    message: 'User deleted',
+                    request: {
+                        type: 'POST',
+                        url: 'http://localhost:3000/api/user',
+                        body: { email: 'String', password: 'String' }
+                    }
+                });
+            })
+            .catch(error => {
+                res.status(500).json({
+                    error: error
+                });
+            });
+    }
+
+    getAllUsers(req, res, next) {
+        User.find()
+            .select('_id email')
+            .exec()
+            .then(docs => {
+                const response = {
+                    count: docs.length,
+                    users: docs.map(doc => {
+                        return {
+                            _id: doc._id,
+                            email: doc.email,
+                            request: {
+                                type: 'GET',
+                                url: 'http://localhost:3000/api/user/' + doc._id
+                            }
+                        }
+                    })
+                };
+                res.status(200).json(response);
+            })
+            .catch(error => {
+                res.status(500).json({
+                    error: error
+                });
+            });
+    }
+
+    getOneUser(req, res, next) {
+        const id = req.params.userId;
+        User.findById(id)
+            .select('_id email')
+            .exec()
+            .then(doc => {
+                if (doc) {
+                    res.status(200).json({
+                        user: doc,
+                        request: {
+                            type: 'GET',
+                            url: 'http://localhost:3000/api/user'
+                        }
+                    });
+                } else {
+                    res.status(404).json({
+                        message: 'No valid entry found for provided ID'
                     });
                 }
             })
--- backend/api/app/routes/user.js
+++ backend/api/app/routes/user.js
@@ -7,73 +7,13 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const User = require('../db/models/user'); // Ensure this path is correct
 const jwt = require('jsonwebtoken');
+const { route } = require('./jackets');
 const jsonwebtoken = require('../config').jsonwebtoken;
 
 //signup a user
 router.post('/signup', UsersController.signup);
-
-module.exports = router;
-
-//login a user
-router.post('/login', (req, res, next) => {
-    User.findOne({ email: req.body.email })
-        .exec()
-        .then(user => {
-            if (!user) {
-                return res.status(401).json({
-                    message: 'Auth failed'
-                });
-            }
-            bcrypt.compare(req.body.password, user.password, (err, result) => {
-                if (err) {
-                    return res.status(401).json({
-                        message: 'Auth failed'
-                    });
-                }
-                if (result) {
-                    const token = jwt.sign({
-                        email: user.email,
-                        userId: user._id
-                    }, jsonwebtoken, {
-                        expiresIn: '1h'
-                    });
-                    return res.status(200).json({
-                        message: 'Auth successful',
-                        token: token
-                    });
-                }
-                res.status(401).json({
-                    message: 'Auth failed'
-                });
-            });
-        })
-        .catch(error => {
-            console.log(error);
-            res.status(500).json({
-                error: error
-            });
-        });
-}
-);
-
-// Delete a user by ID
-router.delete('/:userId', (req, res, next) => {
-    const id = req.params.userId;
-    User.deleteOne({ _id: id })
-        .then(result => {
-            res.status(200).json({
-                message: 'User deleted',
-                request: {
-                    type: 'POST',
-                    url: 'http://localhost:3000/user',
-                    body: { email: 'String', password: 'String' }
-                }
-            });
-        })
-        .catch(error => {
-            res.status(500).json({
-                error: error
-            });
-        });
-});
+router.post('/login', UsersController.login);
+router.get('/', UsersController.getAllUsers);
+router.delete('/:userId', UsersController.deleteUser);
+router.get('/:userId', UsersController.getOneUser);
 module.exports = router;
